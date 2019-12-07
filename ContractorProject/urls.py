fa832acdf6762f8c8ee3392876add5a0f022cd66
Rediet8abere <redietaberegesesse@gmail.com>
2019-12-07 13:47:57 -0800

    update pics

--- ContractorProject/urls.py
+++ ContractorProject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from accounts import views as account_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
     path('', include('words.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
