a9e74da06807da081f5542880efaf7979fd017e3
Rediet8abere <redietaberegesesse@gmail.com>
2019-12-08 19:17:32 -0800

    settings

--- ContractorProject/settings.py
+++ ContractorProject/settings.py
@@ -122,16 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static"),
- ]
+# STATICFILES_DIRS = [
+#      os.path.join(BASE_DIR, "static"),
+#  ]
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_TMP = os.path.join(BASE_DIR, 'static')
+# os.makedirs(STATIC_TMP, exist_ok=True)
+# os.makedirs(STATIC_ROOT, exist_ok=True)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
